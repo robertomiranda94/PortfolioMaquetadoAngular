da7b3f08441db1fb18042f0e800723226a62ad45
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-12-23 00:02:41 -0300

    prueba de angular

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { HeaderService } from 'src/app/servicios/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public usuario : Usuario | undefined;
+  public editUsuario : Usuario | undefined;
+  constructor(private headerService : HeaderService) { }
 
   ngOnInit(): void {
+    this.getUser();
+  }
+
+  public getUser(): void {
+    this.headerService.getUser().subscribe({
+      next: (response: Usuario) => {
+        this.usuario = response;
+      },
+      error: (err: HttpErrorResponse) => {
+        alert(`Error: ${err.message}`);
+      }
+    })
   }
 
 }
