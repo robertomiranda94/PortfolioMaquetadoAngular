e4fd19c5f64c4c7bfab113eaa74749ffca1324ee
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-12-24 16:25:10 -0300

    agrega components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/education.ts
@@ -0,0 +1,7 @@
+export interface Education {
+    idEdu:number;
+    tituloEdu:string;
+    fechaEdu:number;
+    descEdu:number;
+    imagenEdu:string;
+}
